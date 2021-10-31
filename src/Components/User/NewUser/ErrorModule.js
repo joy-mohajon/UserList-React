9a1ac51085e577c7bfde59f7f7b03eff26ac38ee
mohajon33 <92334273+mohajon33@users.noreply.github.com>
2021-10-31 10:15:56 +0600

    Adding error module

---
+++ src/Components/User/NewUser/ErrorModule.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "../Button/Button";
+import Card from "../Card/Card";
+
+import styles from "./ErrorModule.module.css";
+
+const ErrorModule = (props) => {
+  return (
+    <div>
+      <div className={styles.backdrop} onClick={props.onConfirm} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.massage}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.onConfirm}>Okey</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+
+export default ErrorModule;
